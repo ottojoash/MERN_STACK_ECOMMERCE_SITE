026f43c098d87bd7faba14d2d4c931027a7c5f1d
otto joash <louisjoshbricks@gmail.com>
2022-10-05 08:29:27 -0400

    index

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import {
+  Home,
+  WishList,
+  ProtectedRoute,
+  AdminProtectedRoute,
+  CartProtectedRoute,
+  PageNotFound,
+  ProductDetails,
+  ProductByCategory,
+  CheckoutPage,
+} from "./shop";
+import { DashboardAdmin, Categories, Products, Orders } from "./admin";
+import { UserProfile, UserOrders, SettingUser } from "./shop/dashboardUser";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* Routing All page will be here */
+const Routes = (props) => {
+  return (
+    <Router>
+      <Switch>
+        {/* Shop & Public Routes */}
+        <Route exact path="/" component={Home} />
+        <Route exact path="/wish-list" component={WishList} />
+        <Route exact path="/products/:id" component={ProductDetails} />
+        <Route
+          exact
+          path="/products/category/:catId"
+          component={ProductByCategory}
+        />
+        <CartProtectedRoute
+          exact={true}
+          path="/checkout"
+          component={CheckoutPage}
+        />
+        {/* Shop & Public Routes End */}
+
+        {/* Admin Routes */}
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard"
+          component={DashboardAdmin}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/categories"
+          component={Categories}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/products"
+          component={Products}
+        />
+        <AdminProtectedRoute
+          exact={true}
+          path="/admin/dashboard/orders"
+          component={Orders}
+        />
+        {/* Admin Routes End */}
+
+        {/* User Dashboard */}
+        <ProtectedRoute
+          exact={true}
+          path="/user/profile"
+          component={UserProfile}
+        />
+        <ProtectedRoute
+          exact={true}
+          path="/user/orders"
+          component={UserOrders}
+        />
+        <ProtectedRoute
+          exact={true}
+          path="/user/setting"
+          component={SettingUser}
+        />
+        {/* User Dashboard End */}
+
+        {/* 404 Page */}
+        <Route component={PageNotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default Routes;
