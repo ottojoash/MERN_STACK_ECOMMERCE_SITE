a599edf38277211d0159137db26f1f1209276f86
otto joash <louisjoshbricks@gmail.com>
2022-10-06 07:31:42 -0400

    data

---
+++ client/src/components/shop/partials/Actions.js
@@ -0,0 +1,7 @@
+export const logout = () => {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("cart");
+    localStorage.removeItem("wishList");
+    window.location.href = "/";
+  };
+  
